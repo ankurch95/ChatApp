e6022fbdc689ae16816b574859d59b2d9925d8c2
Ankur Chauhan <ankur0660341@gmail.com>
2024-04-22 21:53:26 +0530

    Design Profile screen

--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -1,8 +1,8 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { styles } from './styles'
-import { Typography } from '../../components'
+import { CustomIcon, Typography } from '../../components'
 import { VStack, Box, Divider, Card } from 'native-base';
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
@@ -36,10 +36,101 @@ export const ProfileScreen = () => {
           backgroundColor={'white'}
           margin={2}
           padding={3}>
-          <Typography variant='body'>
-            {username}
-          </Typography>
-          <Divider />
+
+          <View style={{ flexDirection: 'row' }}>
+            <View style={{ height: 60, width: 60, borderRadius: 60 / 2, borderColor: 'grey', borderWidth: 1,alignItems:'center',justifyContent:'center' }}>
+              {/* <Image
+                source={{ uri: "https://wallpaperaccess.com/full/317501.jpg" }}
+                style={{ height: 60, width: 60, borderRadius: 60 / 2 }}
+              /> */}
+              <CustomIcon
+                name='camera-outline' color={'grey'} size={7} />
+            </View>
+            <View style={{ marginLeft: 20 }}>
+              <Typography variant='bigText'>
+                {username}
+              </Typography>
+              <Typography variant='info'>
+                Add About
+              </Typography>
+            </View>
+          </View>
+          <Divider marginY={3} />
+          <View style={{ flexDirection: 'row' }}>
+            <CustomIcon
+              name='pencil' color={'black'} size={5} />
+            <View style={{ marginLeft: 15 }}>
+              <Typography variant='info'>
+                Edit Profile
+              </Typography>
+            </View>
+          </View>
+        </Box>
+
+
+        <Box
+          borderX="1"
+          borderRadius="2xl"
+          backgroundColor={'white'}
+          margin={2}
+          padding={3}>
+          <View style={{ flexDirection: 'row' }}>
+            <CustomIcon
+              name='key-variant' color={'black'} size={5} />
+            <View style={{ marginLeft: 15 }}>
+              <Typography variant='info'>
+                Account
+              </Typography>
+            </View>
+          </View>
+          <Divider marginY={3} />
+          <View style={{ flexDirection: 'row' }}>
+            <CustomIcon
+              name='lock-outline' color={'black'} size={5} />
+            <View style={{ marginLeft: 15 }}>
+              <Typography variant='info'>
+                Privacy
+              </Typography>
+            </View>
+          </View>
+          <Divider marginY={3} />
+          <View style={{ flexDirection: 'row' }}>
+            <CustomIcon
+              name='chat-outline' color={'black'} size={5} />
+            <View style={{ marginLeft: 15 }}>
+              <Typography variant='info'>
+                Chat
+              </Typography>
+            </View>
+          </View>
+        </Box>
+
+
+        <Box
+          borderX="1"
+          borderRadius="2xl"
+          backgroundColor={'white'}
+          margin={2}
+          padding={3}>
+          <View style={{ flexDirection: 'row' }}>
+            <CustomIcon
+              name='information-outline' color={'black'} size={5} />
+            <View style={{ marginLeft: 15 }}>
+              <Typography variant='info'>
+                Help
+              </Typography>
+            </View>
+          </View>
+          <Divider marginY={3} />
+          <View style={{ flexDirection: 'row' }}>
+            <CustomIcon
+              name='share-variant' color={'black'} size={5} />
+            <View style={{ marginLeft: 15 }}>
+              <Typography variant='info'>
+                Share
+              </Typography>
+            </View>
+          </View>
         </Box>
       </ScrollView>
     </SafeAreaView>
